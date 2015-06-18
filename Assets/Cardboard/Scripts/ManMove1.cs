40ce0cbb00d36f3aca8089e6f1e1c513b197f2e8
Naman <Naman13064@iiitd.ac.in>
2015-06-18 23:01:32 +0530

    New Beginning
    Former-commit-id: e346734528487252c0afa58cf05769cedc80d90e
    Former-commit-id: 27659950a8ffa4f4c7d7b043a616c632e0957012

--- Assets/Cardboard/Scripts/ManMove1.cs
+++ Assets/Cardboard/Scripts/ManMove1.cs
@@ -1,37 +1,40 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+namespace UnityStandardAssets.Characters.ThirdPerson
+{
 public class ManMove1 : MonoBehaviour {
+		public ThirdPersonCharacter character;
 
-	public Rigidbody man;
-	public Rigidbody man2;
+		void Start(){
+			character = GetComponent<ThirdPersonCharacter>();
+		}
 
-	void Update () {
-		CollisionDetection ();
-		man.transform.position += new Vector3(0.01f,0.0f,0.0f);
-	}
-
-	void CollisionDetection(){
-		int A = 2000;
-		float B = 0.08f;
-		float e = 2.71f; 
-		float M = 80.0f;
-		Vector3 dx = man.position - man2.position;
-		float mag = dx.magnitude;
-		
-		Vector3 normal = dx/mag; //Normal Vector in the direction from man2 -> man1
-		Vector3 F = M *A * Vector3.Scale(new Vector3(Mathf.Pow (e,(mag / B)),0.0f, 0.0f), normal); //this is wrong, I want to do something like this F = sigma(A * exp(dx/B) * normal) 
-		// 						Sum of all forces due to every person around. In this case, just on person.
-		print(F);
-		man.AddForce (F);
-
-		man2.AddForce (-1*F);
-	}
-
-	void OnTriggerEnter (Collider other)
-	{
-		if(other.gameObject.CompareTag("Person")){
-			print ("Collision Detected!");
+		void Update () {
+		//CollisionDetection ();
+			Vector3 movement = new Vector3(0.0f,0.0f,-1.0f);
+		//	if(character != null)
+				//do your thing with that variable
+				character.Move (movement, false, false);
+		//		else
+		//			Debug.LogError("variableName was null!");			
 		}
 	}
 }
+
+//	void CollisionDetection(){
+//		int A = 2000;
+//		float B = 0.08f;
+//		float e = 2.71f; 
+//		float M = 80.0f;
+//		Vector3 dx = man.position - man2.position;
+//		float mag = dx.magnitude;
+//		Vector3 normal = dx/mag; //Normal Vector in the direction from man2 -> man1
+//		Vector3 F = M *A * Vector3.Scale(new Vector3(Mathf.Pow (e,(mag / B)),0.0f, 0.0f), normal); //this is wrong, I want to do something like this F = sigma(A * exp(dx/B) * normal) 
+		// 						Sum of all forces due to every person around. In this case, just on person.
+//		print(F);
+//		man.AddForce (F);
+
+//		man2.AddForce (-1*F);
+//	}
+
