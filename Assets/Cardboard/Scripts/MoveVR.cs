5bab690e1a7fdada048494178b916aea44f87654
Naman <Naman13064@iiitd.ac.in>
2015-06-19 21:45:43 +0530

    just need to have Social Force now
    Former-commit-id: e4c109c532ca81e5d3a4008611ce758841333599
    Former-commit-id: ef422b819f604317785b318ef67c7cf4f33f5f83

---
+++ Assets/Cardboard/Scripts/MoveVR.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveVR : MonoBehaviour {
+
+	public Rigidbody VR;
+	// Use this for initialization
+	void Start () {
+		VR = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		VR.transform.position += new Vector3 (0.0f, 0.0f, 0.04f);
+	}
+}
