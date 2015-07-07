8ab06334ba4b56951889d3736f68ef4576c9dd48
Naman <namanspace@gmail.com>
2015-07-07 10:45:39 +0530

    social force working now
    Former-commit-id: 39551ec653f6b6869c91151fcbcd4a29cac1e09d
    Former-commit-id: da994474778a9e67d7fbca8e4162dba99e253a2b

---
+++ Assets/Cardboard/Scripts/ManMove3.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//namespace UnityStandardAssets.Characters.ThirdPerson
+//{
+public class ManMove3 : MonoBehaviour {
+	
+	public Rigidbody another;
+	
+	void Start(){
+		
+		another = GetComponent<Rigidbody> ();
+	}
+	
+	void Update () {
+		Vector3 position = new Vector3 (0.01f, 0.0f, 0.0f);;
+		
+		another.position -= position;
+		
+	}
+	
+	
+}
+
+
+
+//}
+
