d44d7414ea7b7ff378fcdd539143b22778188244
Naman <naman13064@iiitd.ac.in>
2016-01-24 15:02:42 +0530

    Final version

--- Assets/Cardboard/Scripts/PersonMovement.cs
+++ Assets/Cardboard/Scripts/PersonMovement.cs
@@ -1,34 +1,16 @@
 ﻿using UnityEngine;
-using System.Collections;
 
 public class PersonMovement : MonoBehaviour {
 	private Rigidbody person;
 	public GameObject PersonGameObject;
-
+	
 	void Start(){
-		//Vector3 direction;
-		//float theta;
-		//theta=Random.Range (-3.142f, 3.142f);
 		person = PersonGameObject.GetComponent<Rigidbody> ();
-		//direction = new Vector3 (Mathf.Cos(theta), 0f, Mathf.Sin(theta));
-		//speed = 1*Time.deltaTime; // m/second
-		//person.velocity = direction * speed;
 	}
 	
 	void Update () {
-
-		// normalize so that fast or slow moving people retain average speed
-		//person.velocity.Normalize ();
-		//person.velocity = person.velocity / person.velocity.magnitude;
-		//person.velocity = person.velocity * speed;
-
-		//person.position += person.velocity;
-
 		person.transform.LookAt (person.position + person.velocity); //look the right way
-		PersonGameObject.GetComponent<Animator> ().speed = 3f * person.velocity.magnitude;
+		PersonGameObject.GetComponent<Animator> ().speed = 2f * person.velocity.magnitude;
 		PersonGameObject.GetComponent<Animator> ().Play ("HumanoidWalk");
-	
 	}
-
-	
 }
--- Assets/Cardboard/Scripts/SocialForce.cs
+++ Assets/Cardboard/Scripts/SocialForce.cs
@@ -1,103 +1,95 @@
 ﻿using UnityEngine;
 using System.Collections;
-using System.Text.RegularExpressions;
 
 public class SocialForce : MonoBehaviour {
 	private Rigidbody[] people;
-	private Rigidbody[] walls;
+	//private Rigidbody[] walls;
 	private Vector3[] goaldir;
 	private int As = 2000;
-	private int Aw = 2000;
-	private float Bs = 0.08f;
-	private float Bw = 0.4f;
+	//private int Aw = 2000;
+	private float Bs = 0.2f;
+	//private double Bw = 0.4;
 	private float M = 70.0f; //kg
-	private float speed = 1f; // m/s
+	private float speed = 0.6f; // m/s
 	private float responseTime=0.1f; // ss
-	private float wallDist_t=0.5f; // m
+	//private double wallDist_t=0.5; // m
 
-	private float periodicX=-9.4f;
-	private float periodicZ=3f;
+	private float periodicX=-7.4f;
+//	private float periodicZ=3f;
 
 	void Start(){
-
-		people = GameObject.Find ("People").GetComponentsInChildren<Rigidbody>();
-		walls = GameObject.Find ("Walls").GetComponentsInChildren<Rigidbody> ();
+		//	Application.targetFrameRate = -1;
+		people = GameObject.Find ("People").GetComponentsInChildren<Rigidbody> ();
+	//	walls = GameObject.Find ("Walls").GetComponentsInChildren<Rigidbody> ();
 		goaldir = new Vector3[people.Length];
 		 
-		int iter=1;
+	/*	int iter = 1;
 		for (int xp=1; xp<8; xp++) {
 			for (int zp=1; zp<8; zp++) {
-				people[iter].position = new Vector3 (-7f+xp/8f*7f*2f, 0f, -3f+zp/8f*3f*2f);
-				iter=iter+1;
+				people [iter].position = new Vector3 (-7f + xp / 8f * 7f * 2f, 0f, -3f + zp / 8f * 3f * 2f);
+				iter = iter + 1;
 			}
 		}
-
+*/
 		foreach (Rigidbody i in people) {
 			if (i.name != "VRPerson") {
 				int idx = System.Array.IndexOf (people, i);
 				float theta;
-				theta=Random.Range (-3.142f/6f, 3.142f/6f);
-				goaldir[idx] = new Vector3 (Mathf.Cos(theta), 0f, Mathf.Sin(theta));
-
+				theta = Random.Range (-3.142f/ 7f, 3.142f / 10f);
+				goaldir [idx] = new Vector3 (Mathf.Cos (theta), 0f, Mathf.Sin (theta));
 			}
-
 		}
-
 	}
-	
+
+
+
 
 	//Called after every 0.02 seconds
 	void FixedUpdate(){
 		foreach (Rigidbody i in people) {
-
 			Vector3 Force = Vector3.zero;
 
 			foreach (Rigidbody j in people) {
-
 				Force += calculateSocialForce (i, j); //returns a Vector3 with Force in some units
-			
-
-			}
-			foreach (Rigidbody j in walls) {
-					
-				Force += calculateWallForce (i, j); //returns a Vector3 with Force in some units
-
 			}
 
+			//foreach (Rigidbody j in walls) {
+		//		Force += calculateWallForce (i, j); //returns a Vector3 with Force in some units
+			//}
 
 			int idx = System.Array.IndexOf (people, i);
-
-			goaldir [idx] = updateGoalDir (i, goaldir [idx]);
+		//	goaldir [idx] = updateGoalDir (i, goaldir [idx]);
 
 			Force += M * (goaldir [idx] * speed - i.velocity) / responseTime;
 			Force.y = 0f;
-			Vector3 suppressY=new Vector3(1f,0f,1f);
+			Vector3 suppressY = new Vector3 (1f, 0f, 1f);
 			if (i.name != "VRPerson") {
 				i.velocity += (Force) / M * Time.deltaTime;
 				i.position += i.velocity * Time.deltaTime;
-				i.position = Vector3.Scale(i.position,suppressY);
+				i.position = Vector3.Scale (i.position, suppressY);
 			}
 
 			if (i.position.x > 7f) {
 				Vector3 position = i.position;
 				position.x = periodicX;
+				position.z = Random.Range(-2f,2f);
 				//if (Random.Range (0f, 1f) < 0.5f) {
 				//	position.z = periodicY;
 				//} else {
 				//	position.z = -periodicY;
 				//}
-				position.y=0;
+				position.y = 0;
 				i.position = position;
 			}
 
 
 		}
-		//System.GC.Collect();
-		//Resources.UnloadUnusedAssets();
 	}
+
+	/*
 	Vector3 updateGoalDir(Rigidbody person, Vector3 goaldir_person){
 
-		float min = 10000f;
+		float min = 10f;
 		foreach(Rigidbody wall in walls){
 			Vector3 tmp = wall.position - person.position;
 			float dx = tmp.magnitude;
@@ -113,7 +105,7 @@ public class SocialForce : MonoBehaviour {
 			return goaldir_person;
 		}
 	}
-
+*/
 	Vector3 calculateSocialForce(Rigidbody target, Rigidbody person){
 		if (target.GetInstanceID () != person.GetInstanceID ()) {
 			
@@ -132,6 +124,7 @@ public class SocialForce : MonoBehaviour {
 			return Vector3.zero;
 		}
 	}
+	/*
 	Vector3 calculateWallForce(Rigidbody target, Rigidbody person){
 		if (target.GetInstanceID () != person.GetInstanceID ()) {
 			
@@ -150,6 +143,7 @@ public class SocialForce : MonoBehaviour {
 			return Vector3.zero;
 		}
 	}
+	 */
 
 
 }
